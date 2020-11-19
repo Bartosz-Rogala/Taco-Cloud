f97be5479b6c3cfa9a3e4ccd82329d101f4eec5b
Bartosz Rogala <Bartosz.j.rogala@gmail.com>
2020-11-19 16:55:11 +0100

    Add database using Spring Data JPA

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,26 +1,47 @@
-// tag::all[]
-// tag::allButValidation[]
 package tacos;
+import java.util.Date;
 import java.util.List;
-// end::allButValidation[]
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-// tag::allButValidation[]
+
 import lombok.Data;
 
+// tag::newFields[]
 @Data
+@Entity
 public class Taco {
 
-    // end::allButValidation[]
-    @NotNull
-    @Size(min=5, message="Name must be at least 5 characters long")
-    // tag::allButValidation[]
-    private String name;
-    // end::allButValidation[]
-    @Size(min=1, message="You must choose at least 1 ingredient")
-    // tag::allButValidation[]
-    private List<String> ingredients;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    private Date createdAt;
+
+//end::newFields[]
+
+    @NotNull(message="Name must be at least 5 characters long")
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
+    @NotNull(message="You must choose at least 1 ingredient")
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+
+  /*
+//tag::newFields[]
+   ...
+
+//end::newFields[]
+   */
+//tag::newFields[]
 }
-//end::allButValidation[]
-//tag::end[]
+//end::newFields[]
--- taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -10,5 +10,4 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
-
 }
